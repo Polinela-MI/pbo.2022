1af6ba0251cd1e554955dd05945204414bf1dd71
nqomariyah <nqomariyah@polinela.ac.id>
2022-11-25 12:19:37 +0700

    template praktikum 13

---
+++ src/modul13/DBExercise.java
@@ -0,0 +1,53 @@
+package modul13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class DBExercise {
+    public static void main(String[] args){
+        System.out.println("Aplikasi Todo List");
+        System.out.println("======================================");
+        System.out.println("todo <item to do> : register todo item");
+        System.out.println("done <no todo> : checklist todo item");
+        System.out.println("list : melihat semua todo item");
+        System.out.println("quit : keluar menu");
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+
+        try{
+            while((line = buff.readLine()) != null){
+                if(line.startsWith("todo")){
+                    saveTodo();
+                } else if(line.startsWith("done")){
+                    checkTodo();
+                } else if(line.startsWith("list")){
+                    listTodo();
+                } else if(line.startsWith("quit")){
+                    break;
+                } else {
+                    throw new IllegalArgumentException("Invalid Command!");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e){
+            System.out.print(e.getMessage());
+        }
+
+    }
+
+    public static void saveTodo(){
+        System.out.println("savetodo");
+    }
+
+    public static void checkTodo(){
+        System.out.println("checktodo");
+    }
+
+    public static void listTodo(){
+        System.out.println("listtodo");
+    }
+}
---
+++ src/modul13/TodoItem.java
@@ -0,0 +1,45 @@
+package modul13;
+
+import java.util.UUID;
+
+public class TodoItem {
+    private String id;
+    private String item;
+    private boolean state;
+
+    public TodoItem(String id, String item, boolean state){
+        this.id = id;
+        this.item = item;
+        this.state = state;
+    }
+
+    public TodoItem(String item){
+        id = UUID.randomUUID().toString();
+        this.item = item;
+        state = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+}
