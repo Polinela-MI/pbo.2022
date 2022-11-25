358a5f3cbeb5448da6ed3d201ca910c41b09fbac
nqomariyah <nqomariyah@polinela.ac.id>
2022-11-25 10:00:25 +0700

    update IOExercise untuk solusi sederhana tugas praktikum

--- src/modul12/IOExercise.java
+++ src/modul12/IOExercise.java
@@ -3,6 +3,7 @@ package modul12;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IOExercise {
@@ -10,28 +11,97 @@ public class IOExercise {
         //file input
         File input = new File("out/input.txt");
 
-        //file output
-        File output = new File("out/output.txt");
+        System.out.println("Pilih tipe output:");
+        System.out.println("1. Text File");
+        System.out.println("2. CSV File");
 
-        String temp;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
+        if(a == 1)
+            toTxt();
+        else
+            toCsv();
+    }
+
+    public static void toTxt(){
+        File input = new File("docs/newfile.txt");
+        File output = new File("docs/out.txt");
+
+        //membaca input
         try {
-            Scanner scan = new Scanner(input);
-
+            Scanner scanner = new Scanner(input);
             PrintWriter writer = new PrintWriter(output);
 
-            while(scan.hasNextLine()){
-                temp = scan.nextLine();//5 7
+            while(scanner.hasNextLine()){
+                String[] hasilSplit = scanner.nextLine().split(" ");
 
-                String[] hasilSplit = temp.split(" ");//[0] = 5, [1] = 7
-                //5 + 7 = 12
-                int hasilPenjumlahan = Integer.parseInt(hasilSplit[0]) + Integer.parseInt(hasilSplit[1]);
+                int result = 0;
+                for(int i = 0; i < hasilSplit.length; i++){
+                    result += Integer.parseInt(hasilSplit[i]);
+                }
 
-                writer.write(hasilSplit[0] + " + " + hasilSplit[1] + " = " + hasilPenjumlahan);
+                String formatOutput = "";
+
+                for(int i = 0; i < hasilSplit.length - 1; i++){
+                    formatOutput += hasilSplit[i] + " + ";
+                }
+                formatOutput += hasilSplit[hasilSplit.length-1] + " = " + result;
+
+                writer.write(formatOutput);
                 writer.write("\n");
-
             }
-            writer.flush();
+
+            writer.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void toCsv(){
+        File input = new File("docs/newfile.txt");
+        File output = new File("docs/out.csv");
+        int max = 0;
+        ArrayList<String> out = new ArrayList<>();
+        //membaca input
+        try {
+            Scanner scanner = new Scanner(input);
+            PrintWriter writer = new PrintWriter(output);
+
+            while(scanner.hasNextLine()){
+                String[] hasilSplit = scanner.nextLine().split(" ");
+                if(hasilSplit.length > max){
+                    max = hasilSplit.length;
+                }
+
+                int result = 0;
+                for(int i = 0; i < hasilSplit.length; i++){
+                    result += Integer.parseInt(hasilSplit[i]);
+                }
+
+                String formatOutput = "";
+
+                for(int i = 0; i < hasilSplit.length; i++){
+                    formatOutput += hasilSplit[i] + ",";
+                }
+                formatOutput += result;
+
+                out.add(formatOutput);
+            }
+
+            char init = 'a';
+            String header = "";
+            for(int j = 0; j < max; j++){
+                header += (char)(init + j) + ",";
+            }
+
+            header += header.replace(","," + ");
+            writer.write(header.substring(0, header.length()-2) + "\n");
+
+            for(String s: out){
+                writer.write(s + "\n");
+            }
+
             writer.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
